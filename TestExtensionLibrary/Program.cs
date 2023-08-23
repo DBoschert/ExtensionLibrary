ad1b82ac94c29091963ecb420eae58c5522709db
Drew Boschert <drew.r.boschert@gmail.com>
2023-08-23 15:52:42 -0400

    Add project files.

---
+++ TestExtensionLibrary/Program.cs
@@ -0,0 +1,29 @@
+﻿using ExtensionLibrary;
+
+var s = "$123,456.78";
+var s1 = "nfkfdkfksdnfke";
+
+s1.WriteLine("The value of s1 is ", " Pretty cool!!!");
+
+Console.WriteLine($"{s} as a decimal is {s.ToDecimal():C}");
+
+int fn = 10;
+Console.WriteLine($"fn {fn} = X^2 + 3X + 5 evaluates to {fn.Eval()}");
+
+
+
+
+
+
+
+
+/*
+
+var s = "kfnGfFGvkHFHfjGdHFHnklvsNk";
+
+var sAllCaps = s.AllCaps();
+var SAllSmall = sAllCaps.AllSmall();
+
+Console.WriteLine($"{s} as all caps is {sAllCaps} and al small is {sAllCaps.AllSmall}");
+
+*/
